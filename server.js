c388587c0274e29fdecfd9f897127929d40bb87d
clandau <so.super@gmail.com>
2018-02-12 01:17:44 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//to allow remote testing by FCC
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus:200}));
+
+//render static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+const listener = app.listen(PORT, () => {
+    console.log('You are listening on port ' + PORT);
+});
